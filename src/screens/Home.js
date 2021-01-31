a74c0aada12ff39f77dc6239339f9ff593762246
Brandon Kelley <bkelley616@gmail.com>
2021-01-31 16:54:48 -0500

    HW5

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -10,57 +10,38 @@ import {
 import {Colors} from '../../styles';
 
 const styles = StyleSheet.create({
-  button1: {
+  button: {
     alignItems: 'center',
     justifyContent: 'center',
-    width: "37%",
-    height: '100%',
-    flex: 1,
+    height: 35,
+    width: 75,
     backgroundColor: Colors.blue,
     borderRadius: 5,
-    margin: 5,
+    margin: 2.5,
   },
-  button2: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: "37%",
-    height: '100%',
-    flex: 2,
-    backgroundColor: Colors.blue,
-    borderRadius: 5,
-    margin: 5,
-    marginLeft: "30%"
-  },
-  button3: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: "37%",
-    height: '100%',
-    flex: 3,
-    backgroundColor: Colors.blue,
-    borderRadius: 5,
-    margin: 5,
-    marginLeft: "60%"
-
-  },
-  
 });
 
 const HomeView = ({navigation}) => {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <View style={{
-        flex: 1,
-        flexDirection: 'column',
-      }}>
-        <TouchableOpacity style={styles.button1} onPress={() => {}}>
-          <Text >Button</Text>
+    <SafeAreaView>
+      <View>
+        <Text>Text 1</Text>
+        <Text>Text 2</Text>
+        <Text>Text 3</Text>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+        }}>
+        <TouchableOpacity style={styles.button} onPress={() => {}}>
+          <Text>Button</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button2} onPress={() => {}}>
-          <Text >Button</Text>
+        <TouchableOpacity style={styles.button} onPress={() => {}}>
+          <Text>Button</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button3} onPress={() => {}}>
-          <Text >Button</Text>
+        <TouchableOpacity style={styles.button} onPress={() => {}}>
+          <Text>Button</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
