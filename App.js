895664b5f0dee8bca8c5cd12a325980f02c5b9ea
Brandon Kelley <bkelley616@gmail.com>
2021-02-04 16:35:06 -0500

    HW10

--- App.js
+++ App.js
@@ -1,36 +1,28 @@
-import React, {useContext, useState} from 'react';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
-
 import {Colors} from './styles';
 import HomeView from './src/screens/Home';
-import HeaderView from './src/screens/Home'
-import {HeaderButtonContextProvider, HeaderButtonContext} from './src/Context';
 
 const Stack = createStackNavigator();
 
 const App = () => {
-
-
   return (
     <NavigationContainer>
-      <HeaderButtonContextProvider>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeView}
-          options={{
-            title: 'HW9',
-            headerStyle: {
-              backgroundColor: Colors.accent,
-            },
-            headerTintColor: Colors.primary,
-            backgroundColor: Colors.primary,
-          }}
-        />
-      </Stack.Navigator>
-      </HeaderButtonContextProvider>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={HomeView}
+            options={{
+              title: 'HW10',
+              headerStyle: {
+                backgroundColor: Colors.accent,
+              },
+              headerTintColor: Colors.primary,
+              backgroundColor: Colors.primary,
+            }}
+          />
+        </Stack.Navigator>
     </NavigationContainer>
   );
 };
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,140 +1,90 @@
-import React, {useContext, useEffect} from 'react';
-import {StyleSheet, TouchableOpacity, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  ScrollView,
+  Button,
+  View,
+  Text,
+  Platform,
+} from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
-import {HeaderButtonContext} from '../Context';
-
-import {Colors} from '../../styles';
 
 const styles = StyleSheet.create({
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 35,
-    width: 120,
-    backgroundColor: Colors.primary,
-    borderRadius: 5,
-    margin: 5,
+  headerText: {
+    fontSize: 30,
+    textAlign: "center",
+    margin: 50,
+    fontWeight: "bold"
   },
-  bigButton: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 35,
-    width: '98%',
-    backgroundColor: Colors.purple,
-    borderRadius: 5,
-    margin: 5,
+  scrollText: {
+    fontSize: 30,
+    textAlign: "center",
+    margin: 50
   },
-  menuButton: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 35,
-    width: 40,
-    backgroundColor: Colors.purple,
-    borderRadius: 5,
-    margin: 10,
-  },
-  buttonCLR: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 35,
-    width: 40,
-    backgroundColor: Colors.red,
-    borderRadius: 5,
-    margin: 12,
-  },
-  hiddenText:{
-    fontSize: 20,
-  }
 });
 
 const HomeView = ({navigation}) => {
-  const [hiddenLocationObject, setHiddenLocationSwitch] = useContext(
-    HeaderButtonContext,
-  );
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <View style={{flex: 1, flexDirection: 'row'}}>
-          <TouchableOpacity
-            style={styles.menuButton}
-            onPress={() => {
-              setHiddenLocationSwitch({...hiddenLocationObject, location1: 1});
-            }}>
-            <Text>1</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.menuButton}
-            onPress={() => {
-              setHiddenLocationSwitch({...hiddenLocationObject, location2: 1});
-            }}>
-            <Text>2</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.menuButton}
-            onPress={() => {
-              setHiddenLocationSwitch({...hiddenLocationObject, location3: 1});
-            }}>
-            <Text>3</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.menuButton}
-            onPress={() => {
-              setHiddenLocationSwitch({...hiddenLocationObject, location4: 1});
-            }}>
-            <Text>4</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.buttonCLR}
-            onPress={() => {
-              setHiddenLocationSwitch({
-                location1: 0,
-                location2: 0,
-                location3: 0,
-                location4: 0,
-              });
-            }}>
-            <Text>CLR</Text>
-          </TouchableOpacity>
-        </View>
-      ),
-    });
-  });
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'android');
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
 
   return (
-    <View
-      style={{
-        display: 'flex',
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-      }}>
-      <View
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-        }}>
-        <TouchableOpacity style={styles.button} onPress={() => {}}>
-          {hiddenLocationObject.location1 ? <Text style={styles.hiddenText}>Top Left</Text> : null}
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => {}}>
-          {hiddenLocationObject.location2 ? <Text style={styles.hiddenText}>Top Right</Text> : null}
-        </TouchableOpacity>
+    <ScrollView>
+       <View>
+      <View>
+        <Text style={styles.headerText}> Date and Time Picker Demo With Scrolling!</Text>
       </View>
-      <View
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-        }}>
-        <TouchableOpacity style={styles.button} onPress={() => {}}>
-          {hiddenLocationObject.location3 ? <Text style={styles.hiddenText}>Bottom Left</Text> : null}
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => {}}>
-          {hiddenLocationObject.location4 ? <Text style={styles.hiddenText}>Bottom Right</Text> : null}
-        </TouchableOpacity>
+      <View>
+        <Text style={styles.scrollText}> Click the button below to open the date picker!</Text>
       </View>
+
+      <View>
+        <Button onPress={showDatepicker} title="Show date picker!"  />
+      </View>
+      <View style={{marginBottom: 200}}></View>
+      <View>
+        <Text style={styles.scrollText}> Keep on scrolling to find the Time Picker!!!!</Text>
+      </View>
+      <View style={{marginBottom: 200}}></View>
+      <View>
+        <Button onPress={showTimepicker} title="Show time picker!"  style={styles.button}/>
+      </View>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
     </View>
+    <View style={{marginBottom: 100}}></View>
+    <View>
+        <Text style={styles.scrollText}> You made it to the bottom!!</Text>
+      </View>
+    </ScrollView>
   );
 };
 
